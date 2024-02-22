6671adcf80fac2d47741344516cabc481bb21493
Tudor <112909151+tudor-seserman@users.noreply.github.com>
2024-02-21 20:55:01 -0600

    Licence and BE user creation/

--- Timeline/Helpers/ApplicationDBContext.cs
+++ Timeline/Helpers/ApplicationDBContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Timeline.Models;
@@ -6,7 +7,6 @@ namespace Timeline.Helpers;
 
 public class ApplicationDBContext : IdentityDbContext<AppUser>
 {
-    // protected readonly IConfiguration Configuration;
 
     public ApplicationDBContext(DbContextOptions dbContextOptions)
         :base(dbContextOptions)
@@ -14,11 +14,26 @@ public class ApplicationDBContext : IdentityDbContext<AppUser>
         
     }
 
-    // protected override void OnConfiguring(DbContextOptionsBuilder options)
-    // {
-    //     // connect to postgres with connection string from app settings
-    //     options.UseNpgsql(Configuration.GetConnectionString("TimelineContext"));
-    // }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        List<IdentityRole> roles = new List<IdentityRole>
+        {
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER"
+            },
+        };
+
+        builder.Entity<IdentityRole>().HasData(roles);
+    }
 
     public DbSet<TEvent> TEvents { get; set; }
 }
